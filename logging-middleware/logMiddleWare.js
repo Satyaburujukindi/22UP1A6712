0a2446467e54cb535c048813194effc021b207a1
Satya Burujukindi <burujukindisrisatya@gmail.com>
2025-09-04 11:31:18 +0530

    modified logmiddleware

--- logging-middleware/logMiddleWare.js
+++ logging-middleware/logMiddleWare.js
@@ -2,13 +2,20 @@ const axios = require("axios");
 
 async function Log(stack, level, pkg, message) {
   try {
+    // enforce lowercase & valid values
+    const allowedStacks = ["backend", "frontend"];
+    const allowedLevels = ["debug", "info", "warn", "error", "fatal"];
+
+    if (!allowedStacks.includes(stack)) throw new Error("Invalid stack");
+    if (!allowedLevels.includes(level)) throw new Error("Invalid level");
+
     const payload = { stack, level, package: pkg, message };
 
-    await axios.post("http://20.244.56.144/test/logs", payload);
+    await axios.post("http://20.244.56.144/evaluation-service/logs", payload);
 
-    console.log('[LOGGED] ${stack} | ${level} | ${pkg} -> ${message}');
-  } catch (error) {
-    console.error("Failed to send log:", error.message);
+    console.log('[LOG SENT]', payload);
+  } catch (err) {
+    console.error("[LOG FAILED]", err.message);
   }
 }
 
